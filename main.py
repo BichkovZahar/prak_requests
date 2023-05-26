ef45cc2e17ccc7e71f928fa852b6fcb01769fa99
Asus <Bichk_di78@student.itstep.org>
2023-05-26 11:05:25 +0300

    1

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+response = requests.get('https://www.example.com/')
+if response.status_code == 200:
+  soup = BeautifulSoup(response.content, "html.parser")
+  title = soup.find('title').text
+  print(title)
