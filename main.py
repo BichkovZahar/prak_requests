08aaefaa912d2b1967ed87a4e48833c7d9c5c8b3
Asus <Bichk_di78@student.itstep.org>
2023-05-27 15:56:51 +0300

    4

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-response = requests.get('https://www.example.com/')
+
+response = requests.get("https://www.example.com")
 if response.status_code == 200:
-  soup = BeautifulSoup(response.content, "html.parser")
-  soup_list = soup.find_all("a")
-  for i in soup_list:
-      href = i.get("href")
-      print(href)
-      if href.startswith("https://"):
-        print(i)
+    soup = BeautifulSoup(response.content, "html.parser")
+    h1 = soup.find_all('h1')
+    for i in h1:
+        print(h1)
 else:
-    print("НЕ має підключення:" , response.status_code)
+    print(f"немає підклчення {response.status_code}")
